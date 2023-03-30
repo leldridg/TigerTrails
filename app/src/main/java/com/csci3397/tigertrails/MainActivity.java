937aa9ad0c9532599de5a77d2a451c7fd8ef78e9
leldridg <leldridg@trinity.edu>
2023-03-30 16:19:25 -0500

    bottom nav bar made, but does not currently do anything

--- app/src/main/java/com/csci3397/tigertrails/MainActivity.java
+++ app/src/main/java/com/csci3397/tigertrails/MainActivity.java
@@ -3,13 +3,17 @@ package com.csci3397.tigertrails;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 //this is a different test
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.getMenu().getItem(1).setEnabled(false);
     }
 }
